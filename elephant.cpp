080b58cd7db96091f8b43f65e48fefa7b02f61fe
Joyonta Kumar Nath <103835569+Joyonta2001@users.noreply.github.com>
2022-04-16 21:40:45 +0600

    elephant
    Codeforces problem 617A Solution

---
+++ elephant.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x,a;
+    cin>>x;
+
+    a = x/5;
+
+    if(x%5==0)
+        cout<<a;
+    else
+        cout<<a+1;
+
+
+  return 0;
+}
