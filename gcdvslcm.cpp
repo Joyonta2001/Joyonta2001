3bb20c0d4fd6f45fe1b5710c7f6b1c24343cfe99
Joyonta Kumar Nath <103835569+Joyonta2001@users.noreply.github.com>
2022-04-16 22:28:32 +0600

    GCD vs LCM
    Codeforces 1665A Solution

---
+++ gcdvslcm.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int t,a,b;
+   cin>>t;
+   for(int i=0; i<t; i++)
+   {
+      int n;
+      cin>>n;
+      
+      a=1;
+      b=n-3;
+      cout<<a<<" "<<b<<" ";
+      cout<<a<<" "<<a;
+      cout<<endl;
+                           
+   }
+
+	return 0;
+}
