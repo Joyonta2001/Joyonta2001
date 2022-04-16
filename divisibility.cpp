9d767d15b5a844fa2cfdbbe7a34120fdd04ffe52
Joyonta Kumar Nath <103835569+Joyonta2001@users.noreply.github.com>
2022-04-16 21:39:11 +0600

    divisibility
    Codeforces Problem 1328A Solution

---
+++ divisibility.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int t;
+   cin>>t;
+   for(int i=0; i<t; i++)
+   {
+      int a,b,x;
+      cin>>a>>b;
+
+     if(a%b==0)
+       cout<<"0"<<endl;
+     else
+       cout<<b-(a%b)<<endl;
+
+
+   }
+
+	return 0;
+}
