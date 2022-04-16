0b56fbc54d38991601abdcea7ecd10dd5db18a71
Joyonta Kumar Nath <103835569+Joyonta2001@users.noreply.github.com>
2022-04-16 22:47:08 +0600

    Word

---
+++ word.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    int upp=0, low=0;
+
+    for(int i=0; i<s.size(); i++)
+    {
+    	if(isupper(s[i]))
+    	{
+             upp++;
+    	}
+    	else
+    	{
+    		low++;
+    	}
+    }
+
+    if(upp>low)
+    {
+    	for(int i=0; i<s.size(); i++)
+    	{
+    		s[i] = toupper(s[i]);
+    	}
+
+    	cout<<s<<endl;
+    }
+
+    if(upp<low)
+    {
+    	for(int i=0; i<s.size(); i++)
+    	{
+    		s[i] = tolower(s[i]);
+    	}
+
+    	cout<<s<<endl;
+    }
+
+    if(upp==low)
+    {
+    	for(int i=0; i<s.size(); i++)
+    	{
+    		s[i] = tolower(s[i]);
+    	}
+
+    	cout<<s<<endl;
+    }
+
+
+	return 0;
+}
