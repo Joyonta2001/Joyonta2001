fdccd1f459f8ea0f2e2ff245ec4b02f45587ebed
Joyonta Kumar Nath <103835569+Joyonta2001@users.noreply.github.com>
2022-04-16 22:42:40 +0600

    Next Round

---
+++ NextRound.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,k,a[100],cnt=0;
+    cin>>n>>k;
+    for(int i=0; i<n; i++)
+    {
+    	cin>>a[i];
+    }
+
+    for(int i=0; i<n; i++)
+    {
+    	if(a[i]>0 && a[i]>=a[k-1])
+    		cnt++;
+    }
+    	
+    
+       cout<<cnt<<endl;
+    
+
+	return 0;
+    
+}
