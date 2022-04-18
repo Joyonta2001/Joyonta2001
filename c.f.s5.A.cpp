11a282c2f066f82939c186561c372e19de0aff6b
Joyonta Kumar Nath <103835569+Joyonta2001@users.noreply.github.com>
2022-04-18 10:39:53 +0600

    Codeforces sheet 5 A Add

---
+++ c.f.s5.A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int f(int a, int b)
+{
+    cout<<a+b<<endl;
+    return 0;
+}
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+
+    f(a,b);
+
+	return 0;
+}
