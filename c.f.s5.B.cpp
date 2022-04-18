c4e1dc2adc2b376c85d5ffbd63153f2532d3ced2
Joyonta Kumar Nath <103835569+Joyonta2001@users.noreply.github.com>
2022-04-18 10:40:47 +0600

    Codeforces sheet 5 B Print

---
+++ c.f.s5.B.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void f(int n)
+{
+    for(int i=1; i<n; i++)
+    {
+        cout<< i << " ";
+    }
+    cout<<n<<endl;
+    return;
+    
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    f(n);
+
+	return 0;
+}
